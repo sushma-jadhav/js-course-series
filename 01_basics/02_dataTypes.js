d5bc19c1f118eae0b0c1e9ebbf97f7760f0439dd
sushma-jadhav <59611909+sushma-jadhav@users.noreply.github.com>
2025-05-16 12:16:51 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //To treat all the old code has newer version
+
+// alert(3 + 3) - will not work has we are using nodejs and not browser
+
+/* console.log(3 +
+    3
+) */ //code readability is must 
+
+// JS Primitive data types
+//     Type	    typeof(return value)	Object wrapper
+    // Null	        "object"	            N/A
+    // Undefined	"undefined"	            N/A
+    // Boolean	    "boolean"	            Boolean
+    // Number	    "number"	            Number
+    // BigInt	    "bigint"	            BigInt
+    // String	    "string"	            String
+    // Symbol	    "symbol"                Symbol
+
+   /*   let str = "string"
+        let num = 123 -> 2 to power 53 is the range available in memory address
+        let bigint -> used for bigger numbers
+        let bool = true
+        null = standalone value or representation of empty value
+        undefinded 
+        symbol = unique
+
+        object
+
+        typeof (undefined) = type of undefined is undefined
+        typeof (null)  = type of null is object */
+    
